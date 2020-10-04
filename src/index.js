3145c3d04dcb3037d7fcb711a02c6f5f88f0f102
Matt Crowder <matt.crowder@veeva.com>
2020-10-04 12:46:57 -0400

    Added home and design guide

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./app"
 import Providers from "./providers"
+import Home from "src/pages/home/home"
 
 ReactDOM.render(
   <React.StrictMode>
     <Providers>
-      <App />
+      <Home />
     </Providers>
   </React.StrictMode>,
   document.getElementById("root"),
