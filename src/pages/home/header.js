30e3284bc3c6c1520a0dc0bd369507b9aa1b2d38
Matt Crowder <matt.crowder@veeva.com>
2020-10-04 13:50:01 -0400

    another header update

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -8,11 +8,18 @@ const Link = styled.div`
   line-height: 16px;
   letter-spacing: -0.107692px;
   color: #9597a5;
+  cursor: pointer;
+  height: 55%;
+  :hover {
+    color: #2d314d;
+    border-bottom: 4px solid rgb(97, 193, 160);
+  }
 `
 const Links = styled.div`
+  height: 100%;
   width: 331px;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   justify-content: space-between;
   ${(props) => props.theme.breakpoints("md")} {
     width: 240px;
